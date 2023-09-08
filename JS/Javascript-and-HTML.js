4d44880317dec29fbb73ad1002618f67df3b4e56
RyanMurray <ryangmurray3@gmail.com>
2023-09-08 10:49:17 -0500

    speed calculation with template literals

--- JS/Javascript-and-HTML.js
+++ JS/Javascript-and-HTML.js
@@ -2,10 +2,11 @@
 
 let myNumber=7;
 
-/*======= PROMPT ====== */
+/*======= ALERT ====== */
 
 //alert("Hello, I am an alert!");
 
+/*======= CONFIRM ====== */
 
 //let userChoice = confirm("Wait! Don't leave yet! Can I chat with you?");
 
@@ -14,4 +15,19 @@ let myNumber=7;
 //}   else {
 //    console.log("booo!");
 //}
+
+/*======= PROMPT ====== */
+
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceLeft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceLeft/currentSpeed) * 60;
+let timeAtPlannedSpeed = (distanceLeft/plannedSpeed) * 60;
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed
+
+
+console.log(`At your current speed you will take: ${timeAtCurrentSpeed.toFixed(0)} minutes`);
+console.log(`At your planned speed you will take: ${timeAtPlannedSpeed.toFixed(0)} minutes.  You will save ${timeSaved.toFixed(0)} minutes`);
+
 
