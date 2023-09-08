ab44295a43a352637868a681f71dcb00d2de5ec6
RyanMurray <ryangmurray3@gmail.com>
2023-09-08 10:22:43 -0500

    cover alert and prompt

---
+++ JS/Javascript-and-HTML.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let myNumber=7;
+
+/*======= PROMPT ====== */
+
+//alert("Hello, I am an alert!");
+
+
+//let userChoice = confirm("Wait! Don't leave yet! Can I chat with you?");
+
+//if(userChoice === true) {
+//    console.log("yay!");
+//}   else {
+//    console.log("booo!");
+//}
+
